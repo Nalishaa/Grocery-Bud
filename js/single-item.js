489a00090c1f771ba8695fdee5f8923e599d626f
Nalishaa <nalishashakya@gmail.com>
2026-01-29 21:13:34 +0545

    feat: Alert when deleting

--- js/single-item.js
+++ js/single-item.js
@@ -1,5 +1,6 @@
 // Create SingleItem Element
 function createSingleItem(item) {
+  
   var $div = $('<div class="single-item"></div>');
 
   $div.html(`
@@ -19,6 +20,12 @@ function createSingleItem(item) {
   $div.find('input[type="checkbox"]').on("change", function () {
     editCompleted(item.id);
   });
+
+    // Add event listener for remove button
+  $div.find(".remove-btn").on("click", function () {
+    removeItem(item.id);
+  });
+
 
   return $div;
 }
